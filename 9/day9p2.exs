7ba8626986668f156a5d44bae835d0dcfba9957c
nb <git@nikolabura.com>
2023-12-10 06:12:58 +0000

    day 9

---
+++ 9/day9p2.exs
@@ -0,0 +1,45 @@
+require IEx
+
+{:ok, contents} = File.read("input.txt")
+
+input =
+  contents
+  |> String.split("\n", trim: true)
+  |> Enum.map(fn s ->
+    s
+      |> String.split(" ", trim: true)
+      |> Enum.map(&String.to_integer/1)
+  end)
+  |> IO.inspect(label: "input")
+  |> Enum.map(fn history ->
+    IO.inspect(history, label: "history")
+    Stream.unfold(history, fn array ->
+      if Enum.all?(array, fn x -> x == 0 end) do
+        nil
+      else
+        {_, differences} = Enum.reduce(array, {nil, []}, fn x, {last_val, differences} ->
+          if last_val == nil do
+            {x, []}
+          else
+            {x, differences ++ [x - last_val]}
+          end
+        end)
+        IO.inspect(differences, label: "differences", charlists: :as_lists)
+        {array, differences}
+      end
+    end) |> Enum.to_list()
+  end)
+  |> IO.inspect()
+  |> Enum.map(fn differences_array ->
+    rev = Enum.reverse(differences_array)
+    Enum.reduce(rev, 0, fn chain, prev_first ->
+      IO.inspect(chain, label: "chain")
+      IO.inspect(prev_first, label: "prev_first")
+      List.first(chain) - prev_first
+    end) |> IO.inspect()
+  end)
+  |> Enum.sum()
+
+IO.inspect(input)
+
+IEx.pry()
