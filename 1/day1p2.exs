c11d35c4d68a0c76a6f6d9991be36d0e77b100c4
nb <git@nikolabura.com>
2023-12-23 22:15:14 +0000

    finally fixed day1 part2 lol

--- 1/day1p2.exs
+++ 1/day1p2.exs
@@ -24,23 +24,24 @@ digits = [
 {:ok, contents} = File.read("input")
 contents
   |> String.split("\n", trim: true)
+  # |> Enum.filter(fn s -> s == "1eight1sdjnfpq" end)
   |> Enum.map(fn s ->
     IO.inspect(s)
     matches =
       Enum.map(digits, fn {matcher, val} ->
         reg = Regex.compile!(matcher)
-          |> Regex.run(s, return: :index)
-        case reg do
-          [{pos, _}] -> {pos, val}
-          nil -> nil
-        end
+          |> Regex.scan(s, return: :index)
+          |> Enum.map(fn [{index, _}] -> {index, val} end)
+        reg
       end)
-      |> Enum.reject(&is_nil/1)
-      #|> IO.inspect()
+      |> List.flatten()
+      |> IO.inspect()
     {{_, first}, {_, last}} = Enum.min_max_by(matches, fn {pos, val} -> pos end) #|> IO.inspect()
     number = first * 10 + last
     IO.puts(number)
+    IO.puts("")
     number
   end)
   |> IO.inspect(limit: :infinity)
   |> Enum.sum()
+  |> IO.inspect(label: "sum")
